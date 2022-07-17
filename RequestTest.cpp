6e574a37ea6f6119ba89711602e15a5e5d2ba5aa
Zhizhou Lu <luzhizhou@hotmail.com>
2022-07-17 04:18:50 -0600

    Interface implementation

--- RequestTest.cpp
+++ RequestTest.cpp
@@ -8,15 +8,30 @@
 
 void RequestTest ::start(const std ::string &uri)
 {
+    m_startTimes.push(std ::chrono ::system_clock ::now());
 }
 void RequestTest ::finish()
 {
+    std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
+    std::chrono::system_clock::time_point startTime = m_startTimes.front();
+    m_startTimes.pop();
+    m_requestTimes.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count());
+    std::sort(m_requestTimes.begin(), m_requestTimes.end());
 }
 unsigned int RequestTest ::mean()
 {
+    unsigned int sum = 0;
+    for (auto &requestTime : m_requestTimes)
+        sum += requestTime;
+    return sum / m_requestTimes.size();
 }
 unsigned int RequestTest ::standardDeviation()
 {
+    unsigned int sum = 0;
+    unsigned int mean = RequestTest::mean();
+    for (auto &requestTime : m_requestTimes)
+        sum += (requestTime - mean) * (requestTime - mean);
+    return sum / m_requestTimes.size();
 }
 
 std::map<int, int> RequestTest::generateHistogram()
@@ -25,8 +40,35 @@ std::map<int, int> RequestTest::generateHistogram()
 }
 std::map<int, int> RequestTest::generateHistogram(int numberOfBins)
 {
+    std::map<int, int> histogram;
+    int minElement = *std::min_element(m_requestTimes.begin(), m_requestTimes.end());
+    int maxElement = *std::max_element(m_requestTimes.begin(), m_requestTimes.end());
+    int binSize = ceil((maxElement - minElement) / numberOfBins);
+
+    for (auto &requestTime : m_requestTimes)
+    {
+        int binNumber = ceil((requestTime - minElement) / binSize);
+        histogram[minElement + binNumber * binSize]++;
+    }
+    int binCount = floor((maxElement - minElement) / binSize);
+
+    for (int i = minElement; i < minElement + binCount * binSize; i += binSize)
+    {
+        if (histogram.count(i) == 0)
+        {
+            return generateHistogram(numberOfBins - 1);
+        }
+    }
+    histogram[minElement + (binCount + 1) * binSize] = 0;
+    return histogram;
 }
 
 void RequestTest::drawHistogram()
 {
+    std::map<int, int> histogram = generateHistogram();
+    std::cout << "Histogram:" << std::endl;
+    for (auto &bin : histogram)
+    {
+        std::cout << bin.first << " - " << bin.first + bin.second << ": " << bin.second << std::endl;
+    }
 }
